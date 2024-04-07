35271daaa67c3a7433ae322452ee7ee1e74f9a9c
AlexSaskevich <saskevich.aleksey@gmail.com>
2024-04-07 14:08:13 +0300

    Add ForceSaveSceneAndProject.cs utility

---
+++ Assets/Source/Scripts/Utils/Editor/ForceSaveSceneAndProject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Source.Scripts.Utils.Editor
+{
+    public class ForceSaveSceneAndProject : MonoBehaviour
+    {
+        [MenuItem("File/Save Scene And Project %#&s")]
+        private static void FunctionForceSaveSceneAndProject()
+        {
+            EditorApplication.ExecuteMenuItem("File/Save");
+            EditorApplication.ExecuteMenuItem("File/Save Project");
+            Debug.Log("Saved scene and project");
+        }
+    }
+}
