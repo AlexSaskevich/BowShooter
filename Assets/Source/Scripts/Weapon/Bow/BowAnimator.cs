1503e25eb3f072eba3eb963fbfeb3ed93e8ec4b0
AlexSaskevich <saskevich.aleksey@gmail.com>
2024-04-16 20:53:53 +0300

    Fix bow animator

--- Assets/Source/Scripts/Weapon/Bow/BowAnimator.cs
+++ Assets/Source/Scripts/Weapon/Bow/BowAnimator.cs
@@ -9,6 +9,8 @@ namespace Source.Scripts.Weapon.Bow
         [SerializeField] private Animator _animator;
         [SerializeField] private float _tensionSpeed;
 
+        private readonly int _tensionHash = Animator.StringToHash(nameof(Tension));
+
         private InputReader _inputReader;
         private float _currentTension;
 
@@ -23,13 +25,12 @@ namespace Source.Scripts.Weapon.Bow
             if (_inputReader.IsFireButtonPressed)
             {
                 _currentTension = TensionCalculator.Calculate(_currentTension, 1, _tensionSpeed);
-                _animator.SetFloat("Tension", _currentTension);
+                _animator.SetFloat(_tensionHash, _currentTension);
             }
             else if (_inputReader.IsFireButtonWasReleased)
             {
                 _currentTension = 0;
-                _animator.SetFloat("Tension", _currentTension);
-                _animator.SetTrigger("Shoot");
+                _animator.SetFloat(_tensionHash, _currentTension);
             }
         }
     }
